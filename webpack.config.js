b626c9fb3b3fb97b26068b197d7a3abc808f6c1a
Tomasz Rydzewski <tomasz.rydzewski@outlook.com>
2020-02-12 23:09:17 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Pomodoro App'
+    })
+  ]
+};
